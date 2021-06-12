5fb72fb487386674e53cb6c02cdcc0ad4369b182
Gustavo Nascimento <gustavoprazeres.nascimento@gmail.com>
2021-06-12 19:54:51 -0300

    Initial commit

---
+++ src/test/java/dio/codeanywhere/libraryapi/ApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.codeanywhere.libraryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
